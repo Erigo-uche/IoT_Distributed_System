03df90147bd520685ea50e148cfa7d51ee5cd8b2
Erigo <ucheerigo@gmail.com>
2026-09-17 10:17:24 -0700

    add more devices for simulation

--- sensor.py
+++ sensor.py
@@ -8,31 +8,37 @@ import paho.mqtt.client as mqtt
 
 MQTT_BROKER = "localhost"
 MQTT_PORT = 1883
-MQTT_TOPIC = "iot/sensors/sensor-001"
+MQTT_TOPIC = "iot/sensors/readings"
 
-DEVICE_ID = "sensor-001"
+DEVICES = [
+    "sensor-001",
+    "sensor-002",
+    "sensor-003",
+]
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
 client.connect(MQTT_BROKER, MQTT_PORT)
 
 while True:
-    temperature = round(random.uniform(20, 35), 2)
-    humidity = round(random.uniform(40, 80), 2)
-
-    reading = {
-        "event_id": str(uuid.uuid4()),
-        "device_id": DEVICE_ID,
-        "temperature": temperature,
-        "humidity": humidity,
-        "timestamp": datetime.now(timezone.utc).isoformat(),
-    }
-
-    payload = json.dumps(reading)
-
-    client.publish(MQTT_TOPIC, payload)
-
-    print(f"Published: {payload}")
+    for device_id in DEVICES:
+        temperature = round(random.uniform(20, 35), 2)
+        humidity = round(random.uniform(40, 80), 2)
+        
+        reading = {
+            "event_id": str(uuid.uuid4()),
+            "device_id": device_id,
+            "temperature": temperature,
+            "humidity": humidity,
+            "timestamp": datetime.now(timezone.utc).isoformat(),
+        }
+        
+        payload = json.dumps(reading)
+        
+        client.publish(MQTT_TOPIC, payload)
+        
+        print(f"Published: {payload}")
+    
 
     time.sleep(5)
     
